848993b60bf68396721b31ddc92a0620448b531b
Student8 <Student8@10.8.33.175>
2016-09-30 13:26:57 -0400

    9/30

---
+++ ChatbotEmerald/src/groupFiles/TobyBot.java
@@ -0,0 +1,37 @@
+package groupFiles;
+
+public class TobyBot {
+	private boolean inSchoolLoop;
+	private String schoolResponse;
+	
+	public void talk() {
+		inSchoolLoop = true;
+		while(inSchoolLoop){
+			EmeraldMain.print("(Type 'quit' to go back.)");
+			//static call on promptInput method from TobyMain class
+			schoolResponse = EmeraldMain.promptInput();
+			if(schoolResponse.indexOf("quit") >=0){
+				inSchoolLoop = false;
+				EmeraldMain.promptForever();
+			}
+			EmeraldMain.print("That's my favorite part about school");
+		}
+	}
+
+	public boolean isTriggered(String userInput) {
+		//String[] triggers = {"school","class","teacher"};
+		
+		if(EmeraldMain.findKeyword(userInput, "school", 0) >=0){
+			return true;
+		}
+
+		if(EmeraldMain.findKeyword(userInput, "class", 0) >=0){
+			return true;
+		}
+		
+		return false;
+	}	
+}
+
+
+

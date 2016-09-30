0a499db17e42ad0f4c903d60e613a62edfd5ccbb
Student8 <Student8@10.8.33.161>
2016-09-30 13:08:10 -0400

    Created Bot

---
+++ ChatbotEmerald/src/groupFiles/KevinBot.java
@@ -0,0 +1,33 @@
+package groupFiles;
+
+public class KevinBot implements Chatbot{
+
+	@Override
+	public void talk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTriggered(String userInput) {
+		String[] triggers = {"hello"};
+		
+		for (int i = 0; i < triggers.length; i++)
+		{
+			if(EmeraldMain.findKeyword(userInput, triggers[i],0)>=0)
+			{
+				return true;
+			}
+				return false;
+		}
+		
+		//duplicate for other trigger words
+		if(EmeraldMain.findKeyword(userInput, "hello", 0)>=0)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+
+}

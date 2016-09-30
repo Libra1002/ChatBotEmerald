2d93389f9a8d2200e24d89529d2fa160328da1a2
Student8 <Student8@10.8.33.177>
2016-09-30 14:02:08 -0400

    asdasdasdasdasdasdasdasd

--- ChatbotEmerald/src/groupFiles/FultonBot.java
+++ ChatbotEmerald/src/groupFiles/FultonBot.java
@@ -1,42 +1,18 @@
 package groupFiles;
 
 public class FultonBot implements Chatbot {
-
-	private boolean inSchoolLoop;
-	private String schoolResponse;
 	
 	public void talk() {
-		inSchoolLoop = true;
-		while(inSchoolLoop){
-			EmeraldMain.print("(Type 'quit' to go back.");
-			//static call on promptInput method from FultonMain class
-			schoolResponse = EmeraldMain.promptInput();
-			if(schoolResponse.indexOf("quit") >= 0){
-				inSchoolLoop = false;
-				EmeraldMain.promptForever();
-			} 
-			EmeraldMain.print("That's my favorite part about school");
-		}
-
+		
 	}
 
 	public boolean isTriggered(String userInput) {
-		String[] triggers = {"school","class","teacher","asdasdasd"};
-		//idea: create a for loop to iterate
-		//through your array to triggers
+		String[] triggers = {""};
 		
-		for(int i = 0; i < triggers.length; i++){
-			if(EmeraldMain.findKeyword(userInput, triggers[i], 0) >= 0){
-			return true;
+		for(int i = 0; i < triggers.length;){
+			if(!(EmeraldMain.findKeyword(userInput, triggers[i], 0) >= 0)){
+			i++;
 			}
-			return false;
-		}
-
-		if(EmeraldMain.findKeyword(userInput, "class", 0) >= 0){
-			return true;
-		}
-
-		if(EmeraldMain.findKeyword(userInput, "school", 0) >= 0){
 			return true;
 		}
 		return false;

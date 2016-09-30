da3f4dc9d9f3ca6408f90d89fd164c2c9fc0da24
Student8 <Student8@10.8.33.175>
2016-09-30 13:32:22 -0400

    023

--- ChatbotEmerald/src/groupFiles/TobyBot.java
+++ ChatbotEmerald/src/groupFiles/TobyBot.java
@@ -19,13 +19,9 @@ public class TobyBot {
 	}
 
 	public boolean isTriggered(String userInput) {
-		//String[] triggers = {"school","class","teacher"};
+		String[] triggers = {"school"};
 		
 		if(EmeraldMain.findKeyword(userInput, "school", 0) >=0){
-			return true;
-		}
-
-		if(EmeraldMain.findKeyword(userInput, "class", 0) >=0){
 			return true;
 		}
 		

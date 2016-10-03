09c0ab275db544a527623d5798d59ea218104913
Toby <thuang7616@bths.edu>
2016-10-03 16:29:53 -0400

    123

--- ChatbotEmerald/src/groupFiles/TobyBot.java
+++ ChatbotEmerald/src/groupFiles/TobyBot.java
@@ -1,25 +1,37 @@
 package groupFiles;
 
 public class TobyBot implements Chatbot {
-	private boolean inSchoolLoop;
-	private String schoolResponse;
+	private String jokeResponse;
+	private boolean inJokeLoop;
+	
+	private int jokeCount;
+	
+	public TobyBot(){
+		jokeCount = 0;
+	}
 	
 	public void talk() {
-		inSchoolLoop = true;
-		while(inSchoolLoop){
+		inJokeLoop = true;
+		while(inJokeLoop){
 			EmeraldMain.print("(Type 'quit' to go back.)");
-			//static call on promptInput method from TobyMain class
-			schoolResponse = EmeraldMain.promptInput();
-			if(schoolResponse.indexOf("quit") >=0){
-				inSchoolLoop = false;
+			jokeResponse = EmeraldMain.promptInput();
+			if(jokeResponse.indexOf("quit") >=0){
+				inJokeLoop = false;
 				EmeraldMain.promptForever();
 			}
-			EmeraldMain.print("That's my favorite part about school");
+			EmeraldMain.print("That's my favorite part about ");
 		}
 	}
 
 	public boolean isTriggered(String userInput) {
 		String[] triggers = {"school"};
+		
+		for(int i = 0; i < triggers.length; ){
+			if(!(EmeraldMain.findKeyword(userInput, triggers[i], 0) >= 0)){
+			i++;
+			}
+			return true;
+		}
 		
 		if(EmeraldMain.findKeyword(userInput, "school", 0) >=0){
 			return true;

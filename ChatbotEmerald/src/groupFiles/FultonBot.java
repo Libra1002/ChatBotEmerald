8e0a9c7465d0ff0989442944bec7c4c1b04ba5e8
Student8 <Student8@10.8.33.177>
2016-10-06 13:18:23 -0400

    asd

--- ChatbotEmerald/src/groupFiles/FultonBot.java
+++ ChatbotEmerald/src/groupFiles/FultonBot.java
@@ -2,12 +2,30 @@ package groupFiles;
 
 public class FultonBot implements Chatbot {
 	
+	private boolean inSongLoop;
+	private boolean visited;
+	public FultonBot(){
+		visited = false;
+	}
+	
 	public void talk() {
-		
+		inSongLoop = true;
+		visited = true;
+		while(inSongLoop){
+			
+			//static call on promptInput method from FultonMain class
+			schoolResponse = EmeraldMain.promptInput();
+			if(schoolResponse.indexOf("quit") >= 0){
+				inSongLoop = false;
+				EmeraldMain.promptFirstQ();
+			} 
+			EmeraldMain.print("That's my favorite part about school");
+		}
+		EmeraldMain.print("(Type 'quit' to go back.");
 	}
 
 	public boolean isTriggered(String userInput) {
-		String[] triggers = {""};
+		String[] triggers = {"Yes","No","Snowman"};
 		
 		for(int i = 0; i < triggers.length; ){
 			if(!(EmeraldMain.findKeyword(userInput, triggers[i], 0) >= 0)){
@@ -16,6 +34,10 @@ public class FultonBot implements Chatbot {
 			return true;
 		}
 		return false;
+	}
+
+	public boolean beenVisited() {
+		return visited;
 	}
 
 }

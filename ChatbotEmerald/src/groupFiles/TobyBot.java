232a4f2f006fecdb056a09a5c446f45756a1674a
Student8 <Student8@10.8.33.175>
2016-10-06 13:56:38 -0400

    123

--- ChatbotEmerald/src/groupFiles/TobyBot.java
+++ ChatbotEmerald/src/groupFiles/TobyBot.java
@@ -23,6 +23,7 @@ public class TobyBot implements Chatbot {
 	public void talk() {
 		inFlirtLoop = true;
 		visited = true;
+		FlirtyBot();
 		while(inFlirtLoop){
 			botResponse();
 			flirtResponse = EmeraldMain.promptInput();
@@ -36,22 +37,20 @@ public class TobyBot implements Chatbot {
 		}	
 	}
 	
-	public static void FlirtyBot() {
+	public void FlirtyBot() {
 
-		inFlirtLoop = true;
-		while(inFlirtyLoop){
-			EmeraldMain.print("Hey there sexy! My name is...actually it's gonna be in your "
-					+ "phone contacts very soon. Want to spare some time to chat with me?");  
-			flirtResponse = EmeraldMain.promptInput();
+		EmeraldMain.print("Hey there sexy! My name is...actually it's gonna be in your "
+				+ "phone contacts very soon. Want to spare some time to chat with me?");  
+		flirtResponse = EmeraldMain.promptInput();
 			
-			if(findKeyword(response, "sure","ok","yes","yeah","yea","yep","okay",0) >=0){
-				EmeraldMain.print("I had a feeling that the bond between us was mutual.");
-			}
+		if(EmeraldMain.findKeyword(flirtResponse, "sure","ok","yes","yeah","yea","yep","okay",0) >=0){
+			EmeraldMain.print("I had a feeling that the bond between us was mutual.");
+		}
 			
-			else{
-				EmeraldMain.print("No worries. If you chat with me for a few minutes, "
-						+ "soon you will be falling in love with me.");
-			}
+		else{
+			EmeraldMain.print("No worries. If you chat with me for a few minutes, "
+				+ "soon you will be falling in love with me.");
+				
 		}
 	}
 
@@ -62,7 +61,7 @@ public class TobyBot implements Chatbot {
 	}
 	
 	private void botSolution() {
-	 .equals( .indexOf()));
+		questionSelect.indexOf();
 	}
 
 	public boolean isTriggered(String userInput) {

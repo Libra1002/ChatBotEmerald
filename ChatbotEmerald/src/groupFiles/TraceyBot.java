ff5a5b6050dedddf8e3d6c8849aa4979ec672d18
zkong7436 <zkong7436@bths.edu>
2016-10-06 22:10:11 -0300

    Update TraceyBot.java
    sdgdga

--- ChatbotEmerald/src/groupFiles/TraceyBot.java
+++ ChatbotEmerald/src/groupFiles/TraceyBot.java
@@ -1,9 +1,11 @@
 package groupFiles;
-
 public class TraceyBot implements Chatbot{
 	
-	private boolean inSchoolLoop;
-	private String schoolResponse;
+	private boolean inLoop;
+	private String shoe;
+	private String color;
+	private String brand;
+	private String response;
 	private boolean visited;
 	public TraceyBot(){
 		visited = false;
@@ -11,31 +13,48 @@ public class TraceyBot implements Chatbot{
 	
 	public void talk() {
 		visited = true;
-		inSchoolLoop=true;
-		while(inSchoolLoop){
-			EmeraldMain.print("(Type quick to go back)");
-			//static call on promptInput method from KongMain class
-			schoolResponse = EmeraldMain.promptInput();
-			if(schoolResponse.indexOf("quit")>=0){
-				inSchoolLoop = false;
-				EmeraldMain.promptForever();
-			}
-			EmeraldMain.print("That's my favorite part about school.");
+		
+		EmeraldMain.print("Nice to meet you, I'm Daniel.");
+		
+		inLoop=true;
+		while(inLoop){
+			EmeraldMain.print(EmeraldMain.user+", what is the color of the shoes you're wearing?");
+			response = EmeraldMain.promptInput();
+			if(isTriggered(response)){
+				color = shoe;
+				inLoop = false;
+			}else{
+				EmeraldMain.print("I don't understand");
+			}			
 		}
+		
+		inLoop=true;
+		while(inLoop){
+			EmeraldMain.print("What is the brand?");
+			response = EmeraldMain.promptInput();
+			if(isTriggered(response)){
+				brand = shoe;
+				inLoop = false;
+			}else{
+				EmeraldMain.print("I don't understand");
+			}	
+		}
+		
+		EmeraldMain.print("Damn "+EmeraldMain.user+", back at it again with the "+color+" "+brand+".");
 		
 	}
 	
 	public boolean isTriggered(String userInput) {
-		String[] trigger = {"school","class","teacher"};
+		String[] trigger = {"Daniel","red","black","white","brown","Nikes","Vans","Adidas","Puma","Converse","Jordans"};
 		
 		for(int i=0; i<trigger.length;i++){
 			if(EmeraldMain.findKeyword(userInput, trigger[i], 0) >= 0){
-			return true;
+				shoe = trigger[i];
+				return true;
 			}
 		
 		} 
 		return false; 
-		//safsdf
 	}
 
 	@Override

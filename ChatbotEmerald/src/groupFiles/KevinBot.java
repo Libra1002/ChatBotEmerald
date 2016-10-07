528f59f6501ba27e0c29c6488ff504587c4f19d5
Libra1002 <kzhen5175@bths.edu>
2016-10-07 00:27:09 -0400

    Update KevinBot.java
    added code

--- ChatbotEmerald/src/groupFiles/KevinBot.java
+++ ChatbotEmerald/src/groupFiles/KevinBot.java
@@ -3,22 +3,44 @@ package groupFiles;
 public class KevinBot implements Chatbot{
 
 	private boolean visited;
+	private boolean jokeLoop;
+	private	String[] jokes = {"Why did the duck cross the road?"};
+	private String[] answers = {"To prove he wasn't a chicken."};
+	private String pun = "ba dum tss";
+	private String jokeResponse;
+
 	public KevinBot()
 	{
 		visited = false;
 	}
 	
-	@Override
 	public void talk() {
 		visited = true;
-		String[] jokes = {"Why did the duck cross the road?"};
-		String[] answers = {"To prove he wasn't a chicken."};
-		String pun = "ba dum tss";
-		// TODO Auto-generated method stub
-		
+		jokeLoop = true;
+		EmeraldMain.print("Well hello there.");
+		while(jokeLoop)
+		{
+			jokeResponse = EmeraldMain.promptInput();
+			EmeraldMain.print("Wanna hear a joke?");
+			
+			if(jokeResponse.indexOf("quit") >= 0)
+			{
+				EmeraldMain.print ("Aw you're no fun....");
+				jokeLoop = false;
+				EmeraldMain.promptFirstQ();
+			} else
+			  {
+			  	int num = (int)(Math.random()*jokes.length);
+				if(jokeResponse.indexOf("yes") >= 0)
+				{
+					EmeraldMain.print(jokes[num]);
+					EmeraldMain.print(answers[num] + ", " + pun);
+					EmeraldMain.print("Wanna hear another?");
+				}
+			  }
+		}
 	}
 
-	@Override
 	public boolean isTriggered(String userInput) {
 		String[] triggers = {"joke"};
 		

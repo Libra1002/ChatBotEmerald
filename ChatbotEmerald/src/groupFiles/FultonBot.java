8caf429ae57c080a655689d570769221e68af7f2
FultonLin <Flin5289@bths.edu>
2016-10-07 00:11:29 -0400

    asd

--- ChatbotEmerald/src/groupFiles/FultonBot.java
+++ ChatbotEmerald/src/groupFiles/FultonBot.java
@@ -2,12 +2,13 @@ package groupFiles;
 
 public class FultonBot implements Chatbot {
 	
+	private boolean inLoop;
 	private boolean inSongLoop;
 	private boolean visited;
 	private String snowmanResponse;
 	private String[] gameSong = {"Do you wanna play a game?","Come on lets go and play",
 			"For the first time in forever","Come out and play","It's like you've gone away...",
-			"We used to be best buddies","And now we're not","I wish you would tell me why!"};
+			"We used to be best buddies","And now we're not","I wish you would tell me why! "};
 	public FultonBot(){
 		visited = false;
 	}
@@ -15,33 +16,42 @@ public class FultonBot implements Chatbot {
 	public void talk() {
 		inSongLoop = true;
 		visited = true;
+		EmeraldMain.print("What song is this a parody of?");
 		EmeraldMain.print(gameSong[0]);
+		songLoop();
 		while(inSongLoop){
 			snowmanResponse = EmeraldMain.promptInput();
-			printResponse();
+			
+			if(snowmanResponse.indexOf("build a snowman") >= 0){
+				EmeraldMain.print("Enjoy the rest of the song ");
+			}
 			
 			if(snowmanResponse.indexOf("quit") >= 0){
 				inSongLoop = false;
 				EmeraldMain.promptFirstQ();
 			} 
 		}
-		EmeraldMain.print("(Type 'quit' to go back.");
 	}
 	
-	private void printResponse() {
-		int count = 1;
-		if(!((EmeraldMain.promptInput()).length() < 0)){
-			EmeraldMain.print(gameSong[count]);
-			
-			while(count <= gameSong.length - 1)
-			{
+	private void songLoop() {
+		inLoop = true;
+		int count = 0;
+		while(inLoop){
 			count++;
+			
+			if(count == gameSong.length-1){
+				inLoop = false;
+			}
+			
+			if(!((EmeraldMain.promptInput()).length() < 0)){
+			EmeraldMain.print(gameSong[count]);
 			}
 		}
+		EmeraldMain.print("Type 'quit' to visit someone else.");
 	}
 
 	public boolean isTriggered(String userInput) {
-		String[] triggers = {"Yes","No","Snowman","Quit"};
+		String[] triggers = {"Yes","No","Snowman"};
 		
 		for(int i = 0; i < triggers.length; ){
 			if(!(EmeraldMain.findKeyword(userInput, triggers[i], 0) >= 0)){

097f2982fc08816b51ad91f31d14c960befbb66f
zkong7436 <zkong7436@bths.edu>
2016-10-06 22:31:10 -0300

    Update TraceyBot.java
    dsfsdfsd

--- ChatbotEmerald/src/groupFiles/TraceyBot.java
+++ ChatbotEmerald/src/groupFiles/TraceyBot.java
@@ -5,10 +5,13 @@ public class TraceyBot implements Chatbot{
 	private String shoe;
 	private String color;
 	private String brand;
+	private String sentence;
+	private int damnCount;
 	private String response;
 	private boolean visited;
 	public TraceyBot(){
 		visited = false;
+		damnCount = 0;
 	};
 	
 	public void talk() {
@@ -40,12 +43,34 @@ public class TraceyBot implements Chatbot{
 			}	
 		}
 		
-		EmeraldMain.print("Damn "+EmeraldMain.user+", back at it again with the "+color+" "+brand+".");
-		
+		sentence = "Damn "+EmeraldMain.user+", back at it again with the "+color+" "+brand+".";
+		EmeraldMain.print(sentence);
+		damnCount++;
+		danielLoop();		
+	}
+	
+	public void danielLoop(){
+		inLoop=true;
+		while(inLoop){
+			int ran = (int)(Math.random()*2);
+			if(ran == 0){
+				EmeraldMain.print("Damn "+EmeraldMain.user);
+			}else{
+				EmeraldMain.print(sentence);
+			}
+			damnCount++;
+			if(damnCount>=6){
+				EmeraldMain.print("(If you're bored, please say quit)");
+			}
+			response = EmeraldMain.promptInput();
+			if (EmeraldMain.findKeyword(response, "quit", 0)>=0){
+				EmeraldMain.promptName();
+			}
+		}
 	}
 	
 	public boolean isTriggered(String userInput) {
-		String[] trigger = {"Daniel","red","black","white","brown","Nikes","Vans","Adidas","Puma","Converse","Jordans"};
+		String[] trigger = {"Daniel","red","black","white","brown","Nikes","Vans","Adidas","Puma","Converse","Jordans","quit"};
 		
 		for(int i=0; i<trigger.length;i++){
 			if(EmeraldMain.findKeyword(userInput, trigger[i], 0) >= 0){
